81db92ab1c4381802e01d6e4fa410afb63ee7f0b
YmClash <momo.toure27@gmail.com>
2023-12-07 19:45:31 +0100

    Day_2

--- Day_2/day_2.py
+++ Day_2/day_2.py
@@ -1,6 +1,6 @@
 
 
-with open('brouillon' ,'r') as file :
+with open('Puzzle_2' ,'r') as file :
     puzz = file.read().strip().split('\n')
 
 
@@ -9,11 +9,12 @@ game_part = []
 for e in puzz:
     part= e.split(';')
     part_2 = part[0].split(':')
-    print(part_2)
-    print(part)
+    # print(part_2)
+    # print(part)
     part[0] = part_2[1]
     print(e)
-    print(part)
+    print(f'{part_2[0]}{part} ')
+    print(len(part))
     # print(len(part))
     # print(e)
     # print(part)
