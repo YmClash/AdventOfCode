6b11bab0e8bfbca2a7c37b52d965e252f395cfb3
YmClash <momo.toure27@gmail.com>
2023-12-08 01:25:30 +0100

    Day_2

--- Day_2/day_2.py
+++ Day_2/day_2.py
@@ -8,29 +8,45 @@ MAX_RED = 12
 MAX_GREEN = 13
 MAX_BLUE = 14
 
-COLOR = ['red','green','blue']
+SAC = {'red':'12','green':'13','blue':'14'}
 
 # print(puzz)
 game_part = []
-for e in puzz:
+Game_IDS = []
+for e  in puzz:
     part= e.split(';')
-    part_2 = part[0].strip().split(':')
+    game_id = part[0].strip().split(':')
     # print(part_2)
     # print(part)       la  je  decoupe,recoupe tout les mots   et les ajoute  dans une liste
-    part[0] = part_2[1]
+    part[0] = game_id[1]
+    Game_IDS.append(game_id[0])
     print(e)
-    # print(f'{part_2[0]}{part} ')
+    # print(f'{game_id[0]}{part} ')
     print(f'Game Part: {part}')
     print(f'Number of set : {len(part)}')
     for o in part:
         series = {}
         cubes = o.split(',')
-        print(cubes)
+        # print(cubes)
         for c in cubes:
             numb,color = c.strip().split(' ')
             series[color] =int(numb)
             print(color)
             print(numb)
+        game_part.append(series)
+
+
+    for series in game_part:
+        for color,numb in series.items():
+            if numb > int(SAC[color]):
+                print("INCOMPATIBLE")
+        print("COMPATIBLE")
+
+
+print(f'GAME PART: {game_part}')
+print(Game_IDS)
+
+
 
     # game_part.append(part)
     # print(game_part)
