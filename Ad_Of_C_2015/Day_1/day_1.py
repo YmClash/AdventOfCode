f2b52139ab1111e2e563a06061e5a845e88a2d61
YmClash <momo.toure27@gmail.com>
2023-12-28 03:18:34 +0100

    day1 2015

--- Ad_Of_C_2015/Day_1/day_1.py
+++ Ad_Of_C_2015/Day_1/day_1.py
@@ -1,31 +1,17 @@
 import itertools
 
-def accumulate(s):
-    total = 0
-    for i in puzzle :
-        if i == "(" :
-            total += 1
-        else :
-            total -= 1
-    return list[total]
-
-def num_etage(s):
-    etage = accumulate(s)
-    return etage[-1],etage.index(-1) +1
-
 
 with open('Puzzle_1','r') as file :
     puzzle = file.read()
 
-total = 0
-etage = 0
+etage= 0
 for i in puzzle:
     if i == "(":
-        total += 1
+        etage += 1
     else:
-        total -= 1
+        etage -= 1
 
-print(f'total de laccumulation : {total}' )
+print(f'Reponse 1 : etage monté : {etage}' )
 
 
 print(len(puzzle))
@@ -33,7 +19,7 @@ print(puzzle)
 
 
 
-etage , num_down_step = num_etage(puzzle)
-print(f'Etage : {etage}')
-print(f'num down step : {num_down_step}')
+# etage , num_down_step = num_etage(puzzle)
+# print(f'Etage : {etage}')
+# print(f'num down step : {num_down_step}')
 
