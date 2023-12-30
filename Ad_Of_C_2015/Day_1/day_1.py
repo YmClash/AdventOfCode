53c09ba1c09d855a51d81ba277a7ce9a542acd34
YmClash <momo.toure27@gmail.com>
2023-12-30 18:11:52 +0100

    day1 2015 solving done + code clean

--- Ad_Of_C_2015/Day_1/day_1.py
+++ Ad_Of_C_2015/Day_1/day_1.py
@@ -1,43 +1,43 @@
 import itertools
 
-
-with open('Puzzle_1','r') as enigme :
+with open('Puzzle_1', 'r') as enigme:
     puzzle = enigme.read()
 
+
 def compteur_etage(file):
-    etage= 0
-    for i in puzzle:
-        if i == "(":
+    etage = 0
+    for i in file :
+        if i == "(" :
             etage += 1
-        else:
+        else :
             etage -= 1
     return etage
+
 
 # def pos_char(file):
 #     pos = [compteur_etage(file)]
 #     return pos[-1], pos.index(-1) + 1
 
-
-
-def floors_pos(file):
+def position_etage(file):
     floors = []
 
-    for o in test:
+    for o in file:
         if o == "(":
             floors.append(1)
-        else:
+        else :
             floors.append(-1)
     return floors
 
-floors = []
+
+pos_etage = []
 
 for s in puzzle:
-    if  s == "(":
-        floors.append(1)
-    else:
-        floors.append(-1)
+    if s == "(":
+        pos_etage.append(1)
+    else :
+        pos_etage.append(-1)
 
-print(floors)
+print(pos_etage)
 #
 # print(f'Etage avant itertool : {floors}')
 # test = list(itertools.accumulate(floors))
@@ -46,44 +46,45 @@ print(floors)
 # print(f"Etage Final: {test[-1]}")
 # print(f"la première position où le personnage atteint le sous-sol: {test.index(-1) + 1}")
 #
-
-
-
+print("---------------------Advent of code 2015--Day 1--------------------------------------")
+print()
+print(puzzle)
+print()
 print("---------------------reponse 1 ----------------------------------------")
 
-print(puzzle)
-print(f'Longeur du puzzle : {len(puzzle)}')
 etage = compteur_etage(puzzle)
-print(f'Etage monté \nResponse 1 : {etage}' )
+
+print(f'Longeur du puzzle : {len(puzzle)}')
+print(f'Etage monté \nResponse 1 : {etage}')
 # print(f'posi={pos}')
 
 print("---------------------reponse 2----------------------------------------")
+
+floors = position_etage(puzzle)
+print(floors)
+print(f'Puzzle Avant Iteration : {floors}')
+floors = list(itertools.accumulate(floors))
+print(f'Puzzle Apres Iteration : {floors}')
+print(f'Etage final : {floors[-1]}')
+print(f'Premier fois a atteindre  le Sous Sol : {floors.index(-1) + 1}')
+print(f'reponse 2 : {floors.index(-1) + 1}')
+
+print("---------------------Test Zone--------------------------------------")
+
+#
+
+# #
+
 print(f"Longueur de Puzzle : {len(puzzle)}")
-print(f'Etage avant itertool : {floors}')
-position = list(itertools.accumulate(floors))
+print(f'Etage avant itertool : {pos_etage}')
+position = list(itertools.accumulate(pos_etage))
 print(f'Etage apres itertool : {position}')
 print(f'Etage : {position[-1]}')
 print(f"Etage Final: {position[-1]}")
 print(f"la première position où le personnage atteint le sous-sol: {position.index(-1) + 1}")
-print(f'Reponse 2 : {position.index(-1) + 1 }')
-
-
-#
-# floors = floors_pos(puzzle)
-# print(puzzle)
-# print(f'Puzzle Avant Iteration : {floors}')
-# floors = list(itertools.accumulate(floors))
-# print(f'Puzzle Apres Iteration : {floors[]}')
-# print(f'Etage final : {floors}')
-# print(f'Premier fois a atteindre  le Sous Sol : {floors.index(-1) +1 }')
-# print(f'reponse : {floors.index(-1) +1}')
-# #
-
-
+print(f'Reponse 2 : {position.index(-1) + 1}')
 print()
-
 
 # etage , num_down_step = num_etage(puzzle)
 # print(f'Etage : {etage}')
 # print(f'num down step : {num_down_step}')
-
