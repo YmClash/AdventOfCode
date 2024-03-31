eb84713a0cb52063b0e33b5bb25c75a3f4288e59
YmClash <momo.toure27@gmail.com>
2024-03-31 21:55:54 +0200

    puzzle day 3

--- Ad_Of_C_2015/Day_3/day3_test.py
+++ Ad_Of_C_2015/Day_3/day3_test.py
@@ -1,3 +1,4 @@
+
 def move(direction:str):
     if direction == '^' :
         print("NORD")
@@ -17,20 +18,29 @@ def move(direction:str):
 
 def santa_move(direction:str,position:list):
     if direction == '^' :
+        print("NORD")
         return (position[0], position[1] + 1)
     elif direction == 'v' :
+        print('SUD')
         return (position[0], position[1] -1 )
     elif direction =='>':
+        print('EST')
         return (position[0] +1,position[1])
     elif direction == '<' :
+        print('Ouest')
         return (position[0]-1,position[1])
     else:
-        raise ValueError('invalid direction '
-                         '')
+        raise ValueError('invalid direction')
 
 
-with open('Puzzle_3','r' ,encoding='utf-8') as file:
-    puzzle = file.read()
 
-    for i in puzzle:
-        print(i,move(i))
+
+with open('input.txt','r') as file:
+    enigme = file.read()
+
+    for i in enigme:
+        move = move(i)
+        print(move)
+
+
+    # print(enigme)
