16a76a1b0ab44993a6447965c2b24f7b57df4db6
YmClash <momo.toure27@gmail.com>
2024-03-31 23:39:26 +0200

    puzzle day 3 solving part 2
    done

--- Ad_Of_C_2015/Day_3/day_3.py
+++ Ad_Of_C_2015/Day_3/day_3.py
@@ -30,6 +30,7 @@ def santa_move(direction:str,position:list):
 def count_maison(directions):
     houses = {}
     position  = (0,0)
+
     houses[position] = 1
     for direction in directions:
         position = santa_move(direction,position)
@@ -38,6 +39,35 @@ def count_maison(directions):
         else:
             houses[position] = 1
     return  len(houses)
+
+
+def count_maison_with_robot(directions) :
+    santa_houses = {}
+    robot_houses = {}
+    santa_position = (0, 0)
+    robot_position = (0, 0)
+    santa_houses[santa_position] = 1
+    robot_houses[robot_position] = 1
+
+
+    for i , direction in enumerate(directions) :
+        if i % 2 == 0:
+            santa_position = santa_move(direction,santa_position)
+            if santa_position in santa_houses:
+                santa_houses[santa_position] += 1
+            else:
+                santa_houses[santa_position] = 1
+
+        else:
+            robot_position = santa_move(direction,robot_position)
+            if robot_position in robot_houses:
+                robot_houses[robot_position] += 1
+            else:
+                robot_houses[robot_position] = 1
+
+    all_houses =  set(santa_houses.keys()).union(set(robot_houses.keys()))
+    return len(all_houses)
+
 
 
 
@@ -51,5 +81,7 @@ with open('Puzzle_3','r' ,encoding='utf-8') as file:
     puzzle = file.read()
     for o in puzzle:
         print(move(o))
-    reponse = count_maison(puzzle)
-    print(f"Reponse part 1 : {reponse} ")
+    reponse_1 = count_maison(puzzle)
+    reponse_2 = count_maison_with_robot(puzzle)
+    print(f"Reponse part 1 : {reponse_1} ")
+    print(f'Reponse part 2 : {reponse_2} ')
