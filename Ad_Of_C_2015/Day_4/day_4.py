63720ef2fad0a953f49f9f901fae507ff760e646
YmClash <momo.toure27@gmail.com>
2024-04-01 05:04:15 +0200

    puzzle day 4
    done

--- Ad_Of_C_2015/Day_4/day_4.py
+++ Ad_Of_C_2015/Day_4/day_4.py
@@ -11,14 +11,18 @@ def hex(key):
     hexa = md.hexdigest()
     return hexa
 
-def bf(key_input):
+
+cle_1 = "00000"
+cle_2 = "000000"
+
+def bf(key_input,cle:str):
     brute_force = True
     subkey = 0
 
     while brute_force:
      key_input = key + str(subkey)
      hash_resultat = hex(key_input.encode())
-     if hash_resultat.startswith("00000"):
+     if hash_resultat.startswith(cle):
          # print(subkey)
          brute_force = False
          return subkey
@@ -29,12 +33,24 @@ print("Reponse part 1 : ")
 print(f'Secret key : {key}')
 haxa = hex(key.encode())
 print(f'Secret key hexadecimal: {haxa}')
-hash_key = bf(key)
+hash_key = bf(key,cle=cle_1)
 print(f'Hash key : {hash_key}')
 reponse = key+str(hash_key)
 print("Reponse :",reponse)
 new_hexa = hex(reponse.encode())
 print("New Hex :",new_hexa)
+print("\n")
+
+print("Reponse part 2 : ")
+print(f'Secret key : {key}')
+haxa_2 = hex(key.encode())
+print(f'Secret key hexadecimal: {haxa_2}')
+hash_key_2 = bf(key,cle=cle_2)
+print(f'Hash key : {hash_key_2}')
+reponse_2 = key+str(hash_key_2)
+print("Reponse :",reponse)
+new_hexa_2 = hex(reponse_2.encode())
+print("New Hex :",new_hexa_2)
 
 
 
