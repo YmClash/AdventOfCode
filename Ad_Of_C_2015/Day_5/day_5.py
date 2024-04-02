88bae827c5e885f7292f70a596b00412edadb17b
YmClash <momo.toure27@gmail.com>
2024-04-03 01:15:40 +0200

    solving Day_5

--- Ad_Of_C_2015/Day_5/day_5.py
+++ Ad_Of_C_2015/Day_5/day_5.py
@@ -1,22 +1,37 @@
 from re import search
 
 
-text  = "Hello World 9"
-pattern = r'[\d+]'
-
-match = search(pattern,text)
-if match:
-    print("Match Found")
-else:
-    print('Match not Found')
+# text  = "Hello World 9"
+# text_2 = "toure@mohamed.mc"
+# pattern = r'[\d+]'
+# yes_or_not = '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+#
+#
+# match = search(yes_or_not,text_2)
+# if match:
+#     print("Match Found")
+# else:
+#     print('Match not Found')
 
 
 def nice_or_not(character:str):
-    pattern_1 = "([aeiou].*){3}"
-    pattern_2 = r"(.)\1"
-    pattern_3 = "ab|cd|pq|xy"
+    pattern_1 = search("([aeiou].*){3}",character)
+    pattern_2 = search(r"(.)\1",character)
+    pattern_3 = search("ab|cd|pq|xy",character)
+    # pattern_uni = if search(pattern_1,character) and search(pattern_2,character) and not search(pattern_3,character):
 
-    match = search()
+
+    match =  search(pattern_1,character) and search(pattern_2,character) and not pattern_3
+    if match:
+        return ("NICE")
+    else:
+        return ("Match not found")
+
+
+match = 0
+unmatch = 0
+MATCH = []
+UNMATCH = []
 
 
 
@@ -26,5 +41,24 @@ with open('test.txt','r') as file:
 
 for i ,line in enumerate(puzzle,start=1):
     print(f'{i} : {line}')
+
+    # verdict = nice_or_not(line)
+    if search("([aeiou].*){3}",line) and search(r"(.)\1",line) and not  search("ab|cd|pq|xy",line):
+        print("Nice")
+        match += 1
+        MATCH.append(line)
+    else:
+        print("Unmatch")
+        unmatch += 1
+        UNMATCH.append(line)
+
+
+print(f'match: {match}')
+print(f'unmatch: {unmatch}')
+print(MATCH)
+print("MATCH LENGHT:",len(MATCH))
+print(UNMATCH)
+print("UNMATCH LENGHT :",len(UNMATCH))
+
 # print(puzzle)
 print(len(puzzle))
