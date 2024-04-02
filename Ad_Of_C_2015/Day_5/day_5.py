e39200a5490dcb68e81a8abbd19db1974932c071
YmClash <momo.toure27@gmail.com>
2024-04-02 23:44:56 +0200

    solving Day_5

--- Ad_Of_C_2015/Day_5/day_5.py
+++ Ad_Of_C_2015/Day_5/day_5.py
@@ -1,6 +1,26 @@
 from re import search
 
-with open('input.txt','r') as file:
+
+text  = "Hello World 9"
+pattern = r'[\d+]'
+
+match = search(pattern,text)
+if match:
+    print("Match Found")
+else:
+    print('Match not Found')
+
+
+def nice_or_not(character:str):
+    pattern_1 = "([aeiou].*){3}"
+    pattern_2 = r"(.)\1"
+    pattern_3 = "ab|cd|pq|xy"
+
+    match = search()
+
+
+
+with open('test.txt','r') as file:
     puzzle = file.read().splitlines()
     # print(puzzle)
 
