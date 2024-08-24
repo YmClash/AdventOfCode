242a85eec3a2c86828b4ce46e5313507d87d36bb
y_mc <momo.toure27@gmail.com>
2024-08-24 13:06:47 +0200

    Solvig  Day 2 in Rust

--- Ad_Of_C_2015/RUST/Day_2/src/main.rs
+++ Ad_Of_C_2015/RUST/Day_2/src/main.rs
@@ -5,7 +5,7 @@ use ymcrust::{read_file,};
 fn main() {
     println!("Hello, world!");
 
-    let file = r"C:\Users\y_mc\RustroverProjects\AdventOfCode\Ad_Of_C_2015\RUST\Day_2\src\test.txt";
+    let file = r"C:\Users\y_mc\RustroverProjects\AdventOfCode\Ad_Of_C_2015\RUST\Day_2\src\input.txt";
     let puzzle = read_file(file);
     let puzzle = puzzle.unwrap();
     println!("Bienvenue a Advent of Code 2015");
@@ -15,19 +15,19 @@ fn main() {
     let mut paperdim:Vec<i32> = Vec::new();
 
 /////////////////////////////////////////////////////////////////////
-    // for i in 0..puzzle.lines().count() {
-    //     let mut _dim: Vec<i32> = Vec::new();
-    //     for line in puzzle.lines() {
-    //         for word in line.split('x') {
-    //             let word = word.parse::<i32>().unwrap();
-    //             _dim.push(word);
-    //             println!("Dimension: {:?}", _dim);
-    //         }
-    //
-    //     }
-    // }
+    for i in 0..puzzle.lines().count() {
+        let mut _dim: Vec<i32> = Vec::new();
+        for line in puzzle.lines() {
+            for word in line.split('x') {
+                let word = word.parse::<i32>().unwrap();
+                _dim.push(word);
+                println!("Dimension: {:?}", _dim);
+            }
+
+        }
+    }
 ////////////////////////////////////////////////////////////
-    // }
+    }
     // for line in puzzle.lines(){
     //     let _dim:Vec<i32> = Vec::new();
     //     for word in line.split('x'){
@@ -39,28 +39,25 @@ fn main() {
 
 
 
+    //
+    // for line in puzzle.lines(){
+    //     for word in line.split('x'){
+    //         let word = word.parse::<i32>().unwrap();
+    //         paperdim.push(word);
+    //     }
+    // }
 
-    for line in puzzle.lines(){
-        for word in line.split('x'){
-            let word = word.parse::<i32>().unwrap();
-            paperdim.push(word);
-        }
-    }
+    // println!("Dimension: {:?}",paperdim);
+    // let smallest = smallest_side(paperdim[0],paperdim[1],paperdim[2]);
+    // let surface = surface_area(paperdim[0],paperdim[1],paperdim[2]);
+    // let total = surface + smallest;
+    //
+    // println!("Surface: {}",surface);
+    // println!("Smallest Side : {}",smallest);
+    // println!("Total: {}",total);
+    //
+    //
 
-    println!("Dimension: {:?}",paperdim);
-    let smallest = smallest_side(paperdim[0],paperdim[1],paperdim[2]);
-    let surface = surface_area(paperdim[0],paperdim[1],paperdim[2]);
-    let total = surface + smallest;
-
-    println!("Surface: {}",surface);
-    println!("Smallest Side : {}",smallest);
-    println!("Total: {}",total);
-
-
-
-
-
-}
 
 fn surface_area(longueur:i32, largeur:i32, hauteur:i32) -> i32{
     let surface = 2*longueur*largeur + 2*largeur*hauteur + 2*hauteur*longueur;
